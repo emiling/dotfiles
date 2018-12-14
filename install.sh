dda8394726a7d07b55fe45b2edec69e0c2643235
emiling <scopradical@gmail.com>
2018-12-14 22:52:09 +0900

    1. nvm, node, yarn 설치 쉘 스크립트 작성 2. zsh 및 apt-get 저장소 추가하는 쉘 스크립트 작성하기

--- install.sh
+++ install.sh
@@ -2,3 +2,18 @@
  # https://github.com/amix/vimrc
  git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
  sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+ # TODO: zsh 설치 추후에 추가하기 
+ # TODO: apt-get 저장소 변경(ftp.daumkakao.com)
+
+
+ echo "nvm과 node를 설치합니다"
+ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+ nvm install --lts
+
+ echo "yarn을 설치합니다"
+ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+ sudo apt-get update && sudo apt-get install yarn
+ echo 'export PATH="$(yarn global bin):$PATH"' >> ~/.zshrc
+
